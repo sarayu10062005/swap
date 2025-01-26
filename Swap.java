609b8774121d96074d3d25f09692373cee3f897d
sarayu10062005 <srijasarayu@gmail.com>
2025-01-26 02:56:03 -0800

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,9 @@
+public class Swap {
+    public static void main(String[] args) {
+        int A = 1, B = 2, temp;
+        temp = A;
+        A = B;
+        B = temp;
+        System.out.printf("A is %d, B is %d",A, B);
+    }
+}
